b861a25ddc37d555d4393f738ec2a26d3a717671
SerenityFlaim <phil_th@mail.ru>
2024-09-20 20:12:49 +0300

    task_1.8 - validation for all fields (tested)

--- Lab-2 - Kernel_classes_and_models/Student.rb
+++ Lab-2 - Kernel_classes_and_models/Student.rb
@@ -1,7 +1,7 @@
 class Student
 
-    attr_accessor :id, :name, :surname, :patronymic, :telegram, :email, :github
-    attr_reader :phone
+    attr_accessor :id
+    attr_reader :phone, :name, :surname, :patronymic, :telegram, :email, :github
 
     #Student constructor
     def initialize(params)
@@ -32,7 +32,27 @@ class Student
     #phone validation with regular expressions
     def self.is_phone_valid?(phone_number)
         phone_pattern = Regexp.compile(/^(?:\+7|8)[\s-]?(?:\(?\d{3}\)?[\s-]?)\d{3}[\s-]?\d{2}[\s-]?\d{2}$/)
-        return phone_pattern.match?(phone_number)
+        return phone_number.nil? || phone_pattern.match?(phone_number)
+    end
+
+    #any type of name validation with regular expressions
+    def self.is_name_valid?(name_string)
+        name_string =~ /^[А-ЯA-Z]{1}[а-яa-z]{1,}(-[А-ЯA-Z]{1}[а-яa-z]{1,})?$/
+    end
+
+    #telegram validation with regular expressions
+    def self.is_telegram_valid?(telegram_tag)
+        telegram_tag.nil? || telegram_tag =~ /^@[\w]+/
+    end
+
+    #email validation with regular expressions
+    def self.is_email_valid?(email_address)
+        email_address.nil? || email_address =~ /^[\w+_.\-]+@[a-zA-Z.\-]+\.[a-zA-Z]{2,}$/
+    end
+
+    #github validation with regular expressions
+    def self.is_github_valid?(github_url)
+        github_url.nil? || github_url =~ %r{^https?://github\.com/[a-zA-Z0-9_\-]+$} #%r allows you to have '/' without escaping them
     end
 
     #phone setter with validation
@@ -43,15 +63,63 @@ class Student
         @phone = phone_number
     end
 
+    #firstname setter with validation
+    def name=(name)
+        if (!self.class.is_name_valid?(name))
+            raise "Firstname isn't stated correctly"
+        end
+        @name = name
+    end
+
+    #surname setter with validation
+    def surname=(surname)
+        if (!self.class.is_name_valid?(surname))
+            raise "Surname isn't stated correctly"
+        end
+        @surname = surname
+    end
+
+    #patronymic setter with validation
+    def patronymic=(patronymic)
+        if (!self.class.is_name_valid?(patronymic))
+            raise "Patronymic isn't stated correctly"
+        end
+        @patronymic = patronymic
+    end
+
+    #telegram setter with validation
+    def telegram=(telegram)
+        if (!self.class.is_telegram_valid?(telegram))
+            raise "Telegram isn't stated correctly"
+        end
+        @telegram = telegram
+    end
+
+    #email setter with validation
+    def email=(email)
+        if (!self.class.is_email_valid?(email))
+            raise "Email isn't stated correctly"
+        end
+        @email = email
+    end
+
+    #github setter with validation
+    def github=(github)
+        if(!self.class.is_github_valid?(github))
+            raise "Github isn't stated correctly"
+        end
+        @github = github
+    end
+
     #Show student data
     def print_info
         puts "<----------------->"
-        puts "Fullname: #{surname} #{name} #{patronymic}"
-        puts "ID: #{@id ? @id : "---"}"
-        puts "Phone: #{@phone ? @phone : "---"}"
-        puts "Telegram: #{@telegram ? @telegram : "---"}"
-        puts "Email: #{@email ? @email : "---"}"
-        puts "Github: #{@github ? @github : "---"}"
+        puts "Fullname:  #{surname} #{name} #{patronymic}"
+        puts "ID:        #{@id ? @id : "---"}"
+        puts "Phone:     #{@phone ? @phone : "---"}"
+        puts "Telegram:  #{@telegram ? @telegram : "---"}"
+        puts "Email:     #{@email ? @email : "---"}"
+        puts "Github:    #{@github ? @github : "---"}"
         puts "<----------------->\n\n"
     end
 
--- Lab-2 - Kernel_classes_and_models/main.rb
+++ Lab-2 - Kernel_classes_and_models/main.rb
@@ -5,22 +5,30 @@ require "./Student.rb"
 # student_3 = Student.new("Nikita", "Smirnov", "Olegovich", 3, "036-738-1441", "@zaiiran", nil, "https://github.com/ZaiiiRan")
 # student_4 = Student.new("Asiet", "Cheush", "Aslanbievna", 4, "10-3971-4623", "@asyanix", nil, "https://github.com/asyanix")
 
-student_1 = Student.new(name: "Philipp", surname: "Matuha", patronymic: "Andreevich", telegram: "@Serenity_flaim", phone: "+7(918)-010-67-15")
+student_1 = Student.new({
+    name: "Philipp", 
+    surname: "Matuha", 
+    patronymic: "Andreevich", 
+    telegram: "@Serenity_flaim", 
+    phone: "+7(918)-010-67-15", 
+    email: "phil_th@mail.ru",
+    github: "https://github.com/SerenityFlaim"
+})
 
-#error will be here
 student_2 = Student.new({
     name: "Sergey",
     surname: "Lotarev", 
     patronymic: "Urievich", 
-    phone: "55-11-6782-8390", 
-    id: 2
+    id: 2,
+    telegram: "@lotarv"
 })
 
 student_3 = Student.new({
     name: "Nikita",
     surname: "Smirnov",
     patronymic: "Olegovich",
-    phone: "89993334545"
+    phone: "89993334545",
+    telegram: "@zaiiran"
 })
 
 student_1.print_info()
